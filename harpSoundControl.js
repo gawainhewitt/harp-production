39538d4276df1ac7aaab6393321d38f9d96247e8
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-09-07 15:12:41 +0100

    testing for playing a given note, passed

--- harpSoundControl.js
+++ harpSoundControl.js
@@ -3,6 +3,12 @@ const Tone = require("./node_modules/tone");
 class HarpSoundControl {
   constructor(Tone) {
     this.Tone = Tone;
+
+    this.chordArray = [
+      ["C3", "E3", "G3", "C4", "E4", "G4", "C5", "E5", "G5", "C6"],
+      ["F3", "A3", "C4", "F4", "A4", "C5", "F5", "A5", "C6", "F6"],
+      ["G3", "B3", "D4", "G4", "B4", "D5", "G5", "B5", "D6", "G6"]
+    ];
   }
 
   startAudio = async function () {
@@ -33,6 +39,13 @@ class HarpSoundControl {
       release: 8,
       volume: -6
     });
+  };
+
+  playNote = (whichString) => {
+    this.sampler.triggerAttack(
+      this.chordArray[whichString.chord][whichString.string],
+      this.Tone.now()
+    );
   };
 }
 
--- unitTests/harpSoundControl.test.js
+++ unitTests/harpSoundControl.test.js
@@ -1,25 +1,29 @@
 const HarpSoundControl = require("../harpSoundControl");
 
-describe("setup audio", () => {
-  const FakeTone = {
-    start: async function () {
-      setTimeout(() => {}, 200);
-    },
-    Sampler: class {
-      constructor(params) {}
-      connect = () => {
-        return "daschunds rule!";
-      };
-      set = () => {};
-    },
-    Reverb: class {
-      constructor(params) {}
-      toDestination = () => {
-        return "blah";
-      };
-    }
-  };
+const FakeTone = {
+  start: async function () {
+    setTimeout(() => {}, 200);
+  },
+  Sampler: class {
+    constructor(params) {}
+    connect = () => {
+      return "daschunds rule!";
+    };
+    set = () => {};
+    triggerAttack = (note, time) => {
+      this.theNote = note;
+    };
+  },
+  Reverb: class {
+    constructor(params) {}
+    toDestination = () => {
+      return "blah";
+    };
+  },
+  now: () => {}
+};
 
+describe("setup audio", () => {
   const harpSoundControl = new HarpSoundControl(FakeTone);
 
   it("console logs 'Audio Started' once audio is started", async () => {
@@ -37,4 +41,15 @@ describe("setup audio", () => {
   it("creates reverb", () => expect(harpSoundControl.reverb).toEqual("blah"));
   it("connect's sampler to reverb", () =>
     expect(harpSoundControl.sampler.connect()).toEqual("daschunds rule!"));
+});
+
+describe("making a sound", () => {
+  const harpSoundControl = new HarpSoundControl(FakeTone);
+
+  it("plays a note", () => {
+    harpSoundControl.setUpSampler();
+
+    harpSoundControl.playNote({ chord: 0, string: 3 });
+    expect(harpSoundControl.sampler.theNote).toEqual("C4");
+  });
 });
