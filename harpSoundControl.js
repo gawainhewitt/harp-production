a0987990a09677ab50d08ee7e8c389fe8d801ec2
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-09-07 13:41:31 +0100

    test that sampler is created is passed

--- harpSoundControl.js
+++ harpSoundControl.js
@@ -1,9 +1,38 @@
 const Tone = require("./node_modules/tone");
 
 class HarpSoundControl {
+  constructor(Tone) {
+    this.Tone = Tone;
+  }
+
   startAudio = async function () {
-    await Tone.start();
+    await this.Tone.start();
     console.log("Audio Started");
+  };
+
+  setUpSampler = async function (callback) {
+    this.sampler = new this.Tone.Sampler({
+      urls: {
+        C4: "Harp-C4.mp3"
+      },
+      baseUrl: "/sounds/",
+      onload: () => {
+        callback();
+      }
+    });
+
+    this.reverb = new this.Tone.Reverb({
+      decay: 3,
+      predelay: 0,
+      wet: 0.5
+    }).toDestination();
+
+    this.sampler.connect(this.reverb);
+
+    this.sampler.set({
+      release: 8,
+      volume: -6
+    });
   };
 }
 
--- unitTests/harpSoundControl.test.js
+++ unitTests/harpSoundControl.test.js
@@ -1,12 +1,33 @@
 const HarpSoundControl = require("../harpSoundControl");
 
 describe("setup audio", () => {
+  const FakeTone = {
+    start: async function () {
+      setTimeout(() => {}, 200);
+    },
+    Sampler: class {
+      constructor(params) {}
+      connect = () => {};
+      set = () => {};
+    },
+    Reverb: class {
+      constructor(params) {}
+      toDestination = () => {};
+    }
+  };
+
+  const harpSoundControl = new HarpSoundControl(FakeTone);
+
   it("console logs 'Audio Started' once audio is started", async () => {
     const logSpy = jest.spyOn(console, "log");
-    const harpSoundControl = new HarpSoundControl();
 
     await harpSoundControl.startAudio();
 
     expect(logSpy).toHaveBeenCalledWith("Audio Started");
+  });
+  it("creates sampler", () => {
+    harpSoundControl.setUpSampler();
+
+    expect(harpSoundControl.sampler).toBeInstanceOf(FakeTone.Sampler);
   });
 });
