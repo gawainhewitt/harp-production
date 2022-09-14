7cdb49346aef9ac3695cca3bedf87ea3b6d0e8ab
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-09-14 21:08:12 +0100

    inserting Tone library to harpSoundControl

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const Tone = require("./node_modules/tone");
+
 const EventBinders = require("./eventBinders");
 const EventHandlers = require("./eventHandlers");
 const HarpSoundControl = require("./harpSoundControl");
 
 const eventBinders = new EventBinders();
-const harpSoundControl = new HarpSoundControl();
+const harpSoundControl = new HarpSoundControl(Tone);
 const eventHandlers = new EventHandlers(eventBinders, harpSoundControl);
